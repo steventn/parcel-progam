faeed6c40d54cc59f29247b721698e5584f95724
Steven Nguyen <steventn96@gmail.com>
2023-05-31 23:46:05 -0700

    added constructor to class objects

--- package.py
+++ package.py
@@ -1,5 +1,8 @@
+import csv
+import hash_table
+
 class Package:
-    def __init__(self, package_id, address, city, zip_code, delivery_deadline, mass, special_notes):
+    def __init__(self, package_id, address, city, zip_code, delivery_deadline, mass, special_notes, delivery_status):
         self.package_id = package_id
         self.address = address
         self.city = city
@@ -7,6 +10,8 @@ class Package:
         self.delivery_deadline = delivery_deadline
         self.mass = mass
         self.special_notes = special_notes
+        self.delivery_status = delivery_status
 
     def __str__(self):
-        return f"{self.package_id}, {self.address}, {self.city}, {self.zip_code}, {self.delivery_deadline}, {self.mass}, {self.special_notes}"
+        return f"{self.package_id}, {self.address}, {self.city}, {self.zip_code}, {self.delivery_deadline}, " \
+               f"{self.mass}, {self.special_notes}, {self.delivery_status} "
--- truck.py
+++ truck.py
@@ -1,8 +1,15 @@
 class Truck:
-    def __init__(self, location, current_time, departed_time):
+    def __init__(self, location, current_time, departed_time, packages, capacity, speed, load, mileage):
         self.location = location
         self.departed_time = departed_time
         self.current_time = current_time
+        self.total_distance = 0
+        self.packages = packages
+        self.capacity = capacity
+        self.speed = speed
+        self.load = load
+        self.mileage = mileage
+
 
     def __str__(self):
         return f"{self.location}, {self.departed_time}, {self.current_time}"
